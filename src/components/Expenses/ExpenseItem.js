a375704186051cff14a26805eb5d7d1b5fa141df
k.b <napsterlv@gmail.com>
2021-08-15 22:51:53 +0300

    event listners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,12 +1,13 @@
+import React, {useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
   
   const clickHandler = () => {
-    title = "Updated";
+    setTitle('Updated');
   };
 
   return (
